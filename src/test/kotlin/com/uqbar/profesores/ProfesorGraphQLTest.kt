4aee325fa14519270a5fc14e31b565fd346f5b78
Fernando Dodino <fernando.dodino@gmail.com>
2022-05-16 00:09:40 -0300

    Adding tests for mutations

--- src/test/kotlin/com/uqbar/profesores/ProfesorGraphQLTest.kt
+++ src/test/kotlin/com/uqbar/profesores/ProfesorGraphQLTest.kt
@@ -4,6 +4,9 @@ import com.jayway.jsonpath.TypeRef
 import com.netflix.graphql.dgs.DgsQueryExecutor
 import com.uqbar.profesores.domain.Materia
 import com.uqbar.profesores.domain.Profesor
+import com.uqbar.profesores.graphql.MateriaInput
+import com.uqbar.profesores.graphql.ProfesoresMutation
+import com.uqbar.profesores.graphql.UpdateProfesor
 import com.uqbar.profesores.repos.MateriaRepository
 import com.uqbar.profesores.repos.ProfesorRepository
 import org.assertj.core.api.Assertions
@@ -11,6 +14,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.annotation.Transactional
 import java.net.URL
 
 @SpringBootTest
@@ -24,6 +28,9 @@ class ProfesorGraphQLTest {
 
     @Autowired
     lateinit var dgsQueryExecutor: DgsQueryExecutor
+
+    @Autowired
+    lateinit var profesoresMutation: ProfesoresMutation
 
     @Test
     fun `consulta de un profesor con sus materias`() {
@@ -61,35 +68,66 @@ class ProfesorGraphQLTest {
         Assertions.assertThat(profesorResult.materias.first().sitioWeb.toString()).contains(profesorPrueba.materias.first().sitioWeb.toString())
     }
 
-//    @Test
-//    @Transactional
-//    fun `podemos actualizar la informacion de un profesor`() {
-//        // Arrange
-//        val profesorId = crearProfesorConMaterias()
-//        val profesorOriginal = getProfesor(profesorId)
-//        val materiaNueva = repoMaterias.save(Materia(nombre = "Ingeniería de Software", sitioWeb = URL("http://ingenieria-software.edu.ar"), cargaHoraSemanal = 5, anio = 5, codigo = "TPI25"))
-//        val cantidadMateriasOriginales = profesorOriginal.materias.size
-//        assertEquals(1, cantidadMateriasOriginales)
-//
-//        // Act
-//        profesorOriginal.agregarMateria(materiaNueva)
-//        updateProfesor(profesorOriginal)
-//
-//        // Assert
-//        val nuevoProfesor = getProfesor(profesorOriginal.id)
-//        val materiasDelProfesorAhora = nuevoProfesor.materias.size
-//        assertEquals(materiasDelProfesorAhora, cantidadMateriasOriginales + 1)
-//    }
+    @Test
+    @Transactional
+    fun `podemos agregar una materia a un profesor por id de materia`() {
+        // Arrange
+        val profesorId = crearProfesorConMaterias()
+        val profesorOriginal = getProfesor(profesorId)
+        val materiaNueva = repoMaterias.save(Materia(nombre = "Ingeniería de Software", sitioWeb = URL("http://ingenieria-software.edu.ar"), cargaHoraSemanal = 5, anio = 5, codigo = "TPI25"))
+        val cantidadMateriasOriginales = profesorOriginal.materias.size
+        Assertions.assertThat(1).isEqualTo(cantidadMateriasOriginales)
 
-//    @Test
-//    fun `no podemos actualizar un profesor inexistente`() {
-//        val idInexistente = 10022L
-//        val profesorBody = mapper.writeValueAsString(Profesor(nombre = "Juan", apellido = "Contardo", anioComienzo = 2013, puntajeDocente = 95))
-//        mockMvc.perform(MockMvcRequestBuilders.put("/profesores/$idInexistente")
-//            .contentType("application/json")
-//            .content(profesorBody)
-//        ).andExpect(MockMvcResultMatchers.status().isNotFound)
-//    }
+        // Act
+        profesorOriginal.agregarMateria(materiaNueva)
+        profesoresMutation.agregarMateria(profesorOriginal.id.toInt(), MateriaInput(materiaNueva.id.toInt(), ""))
+
+        // Assert
+        val nuevoProfesor = getProfesor(profesorOriginal.id)
+        val materiasDelProfesorAhora = nuevoProfesor.materias.size
+        Assertions.assertThat(materiasDelProfesorAhora).isEqualTo(cantidadMateriasOriginales + 1)
+    }
+
+    @Test
+    @Transactional
+    fun `podemos agregar una materia a un profesor por nombre de materia`() {
+        // Arrange
+        val profesorId = crearProfesorConMaterias()
+        val profesorOriginal = getProfesor(profesorId)
+        val materiaNueva = repoMaterias.save(Materia(nombre = "Ingeniería de Software", sitioWeb = URL("http://ingenieria-software.edu.ar"), cargaHoraSemanal = 5, anio = 5, codigo = "TPI25"))
+        val cantidadMateriasOriginales = profesorOriginal.materias.size
+        Assertions.assertThat(1).isEqualTo(cantidadMateriasOriginales)
+
+        // Act
+        profesorOriginal.agregarMateria(materiaNueva)
+        profesoresMutation.agregarMateria(profesorOriginal.id.toInt(), MateriaInput(0, materiaNueva.nombre))
+
+        // Assert
+        val nuevoProfesor = getProfesor(profesorOriginal.id)
+        val materiasDelProfesorAhora = nuevoProfesor.materias.size
+        Assertions.assertThat(materiasDelProfesorAhora).isEqualTo(cantidadMateriasOriginales + 1)
+    }
+
+    @Test
+    @Transactional
+    fun `podemos actualizar los datos de un profesor`() {
+        // Arrange
+        val profesorId = crearProfesorConMaterias()
+        val profesorOriginal = getProfesor(profesorId)
+
+        // Act
+        profesoresMutation.actualizarProfesor(UpdateProfesor(id = profesorOriginal.id.toInt(), nombre = "Ferenc", apellido = "Puszkas"))
+
+        // Assert
+        val nuevoProfesor = getProfesor(profesorOriginal.id)
+        Assertions.assertThat(nuevoProfesor.nombre).isEqualTo("Ferenc")
+        Assertions.assertThat(nuevoProfesor.apellido).isEqualTo("Puszkas")
+        Assertions.assertThat(nuevoProfesor.puntajeDocente).isEqualTo(profesorOriginal.puntajeDocente)
+    }
+
+    /************************************************************************************************
+     *
+     */
 
     private fun buscarProfesor(idProfesor: Long) = dgsQueryExecutor.executeAndExtractJsonPathAsObject("""
                 {
